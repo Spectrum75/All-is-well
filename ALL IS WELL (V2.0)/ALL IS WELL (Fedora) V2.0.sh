abd11e421f53c8731732ec93a8bd58c30e6700f4
Spectrumgamer75 <72115696+spectrumgamer75@users.noreply.github.com>
2020-10-03 13:01:47 +0530

    ALL IS WELL Version 2.0

---
+++ ALL IS WELL (V2.0)/ALL IS WELL (Fedora) V2.0.sh
@@ -0,0 +1,55 @@
+#!/bin/bash      
+echo '
+ 
+                                     O═══════════════════════════════════════════════════════════════════════════════════════O
+                                     ║                                                                                       ║
+                                     ║   █████╗ ██╗     ██╗         ██╗███████╗    ██╗    ██╗███████╗██╗     ██╗         ██╗ ║  
+                                     ║  ██╔══██╗██║     ██║         ██║██╔════╝    ██║    ██║██╔════╝██║     ██║         ██║ ║
+                                     ║  ███████║██║     ██║         ██║███████╗    ██║ █╗ ██║█████╗  ██║     ██║         ██║ ║
+                                     ║  ██╔══██║██║     ██║         ██║╚════██║    ██║███╗██║██╔══╝  ██║     ██║         ╚═╝ ║
+                                     ║  ██║  ██║███████╗███████╗    ██║███████║    ╚███╔███╔╝███████╗███████╗███████╗    ██╗ ║
+                                     ║  ╚═╝  ╚═╝╚══════╝╚══════╝    ╚═╝╚══════╝     ╚══╝╚══╝ ╚══════╝╚══════╝╚══════╝    ╚═╝ ║
+                                     O═══════════════════════════════════════════════════════════════════════════════════════O  '                                       
+                                            
+echo -en '\n'                                           
+echo '                                         #JUST A SMALL SCRIPT FOR EASY UPDATE/UPGRADES ON LINUX (Fedora)'
+echo '                                                                           VERSION 2.0'                                           
+                                            
+                                            
+                                            
+                                                                       
+                                                                                                                                  
+
+                                                                             
+#SPACE
+echo -en '\n'
+#SPACE                                                                                                                                                                                                                                                                                                                   
+echo -en '\n'
+sleep 5
+echo "Please enter your password, if any to start ッ" 
+echo -en '\n'
+#SPACE                                                       
+sudo dnf update --fix-missing && sudo dnf upgrade -y && sudo dnf dist-upgrade -y && sudo dnf autoremove -y
+echo -en '\n'
+#SPACE 
+echo "All is well now, press x to exit 〷◠‿◠〷"
+count=0
+while : ; do
+read -n 1 k <&1
+if [[ $k = x ]] ; then
+printf "\nQuitting from the script\n"
+break
+else
+((count=$count+1))
+printf "\nWrong key pressed for $count times\n"
+echo "Press 'x' to exit (ʘ‿ʘ)"
+fi
+done
+
+# About this Script:
+# ALL IS WELL is a bash script that allows users to easily update and upgrade their repositories and packages on linux. Forget about typing all update/upgrade commands  manually, when ALL IS WELL!
+
+# SCRIPT AUTHOR: SPECTRUMGAMER75 (^̮^)
+# www.github.com/spectrumgamer75
+# Incase of any errors kindly let me know on GitHub.
+
