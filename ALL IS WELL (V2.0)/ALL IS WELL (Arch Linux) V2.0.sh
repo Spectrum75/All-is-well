2c82ef16f6509721edbb73eea2d6ed10ea3211a4
Spectrumgamer75 <72115696+spectrumgamer75@users.noreply.github.com>
2020-10-03 13:31:24 +0530

    Arch Linux

--- ALL IS WELL (V2.0)/ALL IS WELL (Arch Linux) V2.0.sh
+++ ALL IS WELL (V2.0)/ALL IS WELL (Arch Linux) V2.0.sh
@@ -32,6 +32,8 @@ echo -en '\n'
 sudo pacman -Syy && sudo pacman -Su && pacman -Syu && pacman -Qdtq | pacman -Rs - && paccache -r && pacman -Sc && pacman -Scc
 echo -en '\n'
 #SPACE 
+#Put a hash in front of "clear" to stop clearing the output 
+clear
 echo "All is well now, press x to exit 〷◠‿◠〷"
 count=0
 while : ; do
