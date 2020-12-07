16772972ff8a4d3c37fa55886b8f276d00325fd0
Spectrumgamer75 <72115696+spectrumgamer75@users.noreply.github.com>
2020-12-07 08:57:00 +0530

    All Is Well Script For Debian And Ubuntu

--- ALL IS WELL (V2.0)/aiwdebianubuntu.sh
+++ ALL IS WELL (V2.0)/aiwdebianubuntu.sh
@@ -23,7 +23,7 @@ echo "Please enter your password, if any to start ッ"
 echo -en '\n'
 #SPACE                                                       
 sudo apt update --fix-missing && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y
-echo -en && sudo apt install -f && sudo dpkg --configure -a'\n'
+echo -en && sudo apt install -f && sudo dpkg --configure -a
 #SPACE 
 #Put a hash in front of "clear" to stop clearing the output 
 clear
@@ -37,14 +37,15 @@ break
 else
 ((count=$count+1))
 printf "\nWrong key pressed for $count times\n"
-echo "Press 'x' to exit (ʘ‿ʘ)"
+echo "All is well now (if everything went correctly),press x to exit ◠‿◠"
 fi
 done
 
 # About this Script:
 # ALL IS WELL is a bash script that allows users to easily update and upgrade their repositories and packages on linux, BSD, And More. It will also fix any broken packages and dependencies, if possible. Forget about typing all update/upgrade commands  manually, when ALL IS WELL!
 
-
-# SCRIPT AUTHOR: SPECTRUMGAMER75 (^̮^)
-# www.github.com/spectrumgamer75
-# Incase of any errors kindly let me know on GitHub.
+######################################################
+# SCRIPT AUTHOR: SPECTRUMGAMER75 (^̮^)                #
+# www.github.com/spectrumgamer75                     #
+# Incase of any errors kindly let me know on GitHub. #
+######################################################
