3165b67b187b6093a22157432c36a5dee675a73c
Spectrumgamer75 <72115696+spectrumgamer75@users.noreply.github.com>
2020-12-31 19:34:07 +0530

    Added fixes

--- update.sh
+++ update.sh
@@ -3,137 +3,186 @@
 
 rm -r ~/AIW
 echo "UPDATE UTILITY FOR THE 'ALL IS WELL' SCRIPT"
-PS3='Choose your linux distro to update: '
-distros=("Debian/Ubuntu" "Arch Linux" "Fedora" "Cent OS" "Solus OS")
-select fav in "${distros[@]}"; do
-    case $distros in
-        "Debian/Ubuntu")
-            echo "Updating 'All Is Well' Script For Debian/Ubuntu"
-            mkdir ~/AIW
-            cd ~/AIW || error "Failed to enter directory 'AIW'!"
-            wget 'https://raw.githubusercontent.com/spectrumgamer75/Bash-Scripts/master/ALL%20IS%20WELL%20(V2.0)/icon-64.png'
-            wget 'https://raw.githubusercontent.com/spectrumgamer75/Bash-Scripts/master/ALL%20IS%20WELL%20(V2.0)/aiwdebianubuntu.sh'
-            chmod +x 'aiwdebianubuntu.sh'
-            echo "[Desktop Entry]
-            Name=All Is Well
-            GenericName=Update Helper and Fixer
-            Comment=ALL IS WELL is a bash script that allows users to easily update and upgrade their repositories and packages on linux, BSD, And More. It will also fix any broken packages and dependencies, if possible. Forget about typing all update/upgrade commands  manually, when ALL IS WELL!
-            Exec=$HOME/AIW/aiwdebianubuntu.sh
-            Icon=$HOME/AIW/icon-64.png
-            Terminal=true
-            StartupNotify=true
-            Type=Application
-            Categories=Utility;" > ~/.local/share/applications/aiw.desktop || error "Failed to create menu entry!"
-            break
-            ;;
-        "Arch Linux")
-            echo "Updating 'All Is Well' Script For Arch Linux"
-            mkdir ~/AIW
-            cd ~/AIW || error "Failed to enter directory 'AIW'!"
-            wget 'https://raw.githubusercontent.com/spectrumgamer75/Bash-Scripts/master/ALL%20IS%20WELL%20(V2.0)/icon-64.png'
-            wget 'https://raw.githubusercontent.com/spectrumgamer75/Bash-Scripts/master/ALL%20IS%20WELL%20(V2.0)/aiwarchlinux.sh'
-            chmod +x 'aiwarchlinux.sh'
-            echo "[Desktop Entry]
-            Name=All Is Well
-            GenericName=Update Helper and Fixer
-            Comment=ALL IS WELL is a bash script that allows users to easily update and upgrade their repositories and packages on linux, BSD, And More. It will also fix any broken packages and dependencies, if possible. Forget about typing all update/upgrade commands  manually, when ALL IS WELL!
-            Exec=$HOME/AIW/aiwarchlinux.sh
-            Icon=$HOME/AIW/icon-64.png
-            Terminal=true
-            StartupNotify=true
-            Type=Application
-            Categories=Utility;" > ~/.local/share/applications/aiw.desktop || error "Failed to create menu entry!"
-            break
-            ;;
-        "Fedora")
-            echo "Updating 'All Is Well' Script For Fedora"
-            mkdir ~/AIW
-            cd ~/AIW || error "Failed to enter directory 'AIW'!"
-            wget 'https://raw.githubusercontent.com/spectrumgamer75/Bash-Scripts/master/ALL%20IS%20WELL%20(V2.0)/icon-64.png'
-            wget 'https://raw.githubusercontent.com/spectrumgamer75/Bash-Scripts/master/ALL%20IS%20WELL%20(V2.0)/aiwfedora.sh'
-            chmod +x 'aiwfedora.sh'
-            echo "[Desktop Entry]
-            Name=All Is Well
-            GenericName=Update Helper and Fixer
-            Comment=ALL IS WELL is a bash script that allows users to easily update and upgrade their repositories and packages on linux, BSD, And More. It will also fix any broken packages and dependencies, if possible. Forget about typing all update/upgrade commands  manually, when ALL IS WELL!
-            Exec=$HOME/AIW/aiwfedora.sh
-            Icon=$HOME/AIW/icon-64.png
-            Terminal=true
-            StartupNotify=true
-            Type=Application
-            Categories=Utility;" > ~/.local/share/applications/aiw.desktop || error "Failed to create menu entry!"
-            break
-            ;;
-        "Cent OS")
-            echo "Updating 'All Is Well' Script For Cent OS"
-            mkdir ~/AIW
-            cd ~/AIW || error "Failed to enter directory 'AIW'!"
-            wget 'https://raw.githubusercontent.com/spectrumgamer75/Bash-Scripts/master/ALL%20IS%20WELL%20(V2.0)/icon-64.png'
-            wget 'https://raw.githubusercontent.com/spectrumgamer75/Bash-Scripts/master/ALL%20IS%20WELL%20(V2.0)/aiwcentos.sh'
-            chmod +x 'aiwcentos.sh'
-            echo "[Desktop Entry]
-            Name=All Is Well
-            GenericName=Update Helper and Fixer
-            Comment=ALL IS WELL is a bash script that allows users to easily update and upgrade their repositories and packages on linux, BSD, And More. It will also fix any broken packages and dependencies, if possible. Forget about typing all update/upgrade commands  manually, when ALL IS WELL!
-            Exec=$HOME/AIW/aiwcentos.sh
-            Icon=$HOME/AIW/icon-64.png
-            Terminal=true
-            StartupNotify=true
-            Type=Application
-            Categories=Utility;" > ~/.local/share/applications/aiw.desktop || error "Failed to create menu entry!"
-            break
-            ;;
-        "Solus OS")
-            echo "Updating 'All Is Well' Script For Solus OS"
-            mkdir ~/AIW
-            cd ~/AIW || error "Failed to enter directory 'AIW'!"
-            wget 'https://raw.githubusercontent.com/spectrumgamer75/Bash-Scripts/master/ALL%20IS%20WELL%20(V2.0)/icon-64.png'
-            wget 'https://raw.githubusercontent.com/spectrumgamer75/Bash-Scripts/master/ALL%20IS%20WELL%20(V2.0)/aiwsolus.sh'
-            chmod +x 'aiwsolus.sh'
-            echo "[Desktop Entry]
-            Name=All Is Well
-            GenericName=Update Helper and Fixer
-            Comment=ALL IS WELL is a bash script that allows users to easily update and upgrade their repositories and packages on linux, BSD, And More. It will also fix any broken packages and dependencies, if possible. Forget about typing all update/upgrade commands  manually, when ALL IS WELL!
-            Exec=$HOME/AIW/aiwsolus.sh
-            Icon=$HOME/AIW/icon-64.png
-            Terminal=true
-            StartupNotify=true
-            Type=Application
-            Categories=Utility;" > ~/.local/share/applications/aiw.desktop || error "Failed to create menu entry!"
-            break
-            ;;
-        "BSD")
-            echo "Updating 'All Is Well' Script For BSD"
-            mkdir ~/AIW
-            cd ~/AIW || error "Failed to enter directory 'AIW'!"
-            wget 'https://raw.githubusercontent.com/spectrumgamer75/Bash-Scripts/master/ALL%20IS%20WELL%20(V2.0)/icon-64.png'
-            wget 'https://raw.githubusercontent.com/spectrumgamer75/Bash-Scripts/master/ALL%20IS%20WELL%20(V2.0)/aiwbsd.sh'
-            chmod +x 'aiwbsd.sh'
-            echo "[Desktop Entry]
-            Name=All Is Well
-            GenericName=Update Helper and Fixer
-            Comment=ALL IS WELL is a bash script that allows users to easily update and upgrade their repositories and packages on linux, BSD, And More. It will also fix any broken packages and dependencies, if possible. Forget about typing all update/upgrade commands  manually, when ALL IS WELL!
-            Exec=$HOME/AIW/aiwbsd.sh
-            Icon=$HOME/AIW/icon-64.png
-            Terminal=true
-            StartupNotify=true
-            Type=Application
-            Categories=Utility;" > ~/.local/share/applications/aiw.desktop || error "Failed to create menu entry!"
-            break
+#!/usr/bin/env bash
+# THIS IS THE UPDATE SCRIPT FOR THE "ALL IS WELL" SCRIPT.
+echo 'Choose your option below, to continue:
+1 Debian/Ubuntu
+2 Arch Linux
+3 Fedora
+4 Cent OS
+5 Solus OS
+6 BSD
+7 Cancel Update'
+read number && echo "selected"
+case $number in
+    1)
+        echo "Updating All Is Well Script For Debian/Ubuntu"
+        mkdir ~/AIW
+        cd ~/AIW || error "Failed to enter directory 'AIW'!"
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/ALL%20IS%20WELL%20(V2.0)/icon-64.png'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/ALL%20IS%20WELL%20(V2.0)/aiwdebianubuntu.sh'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/uninstall.sh'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/LICENSE'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/update.sh'
+        chmod +x 'uninstall.sh'
+        chmod +x 'aiwdebianubuntu.sh'
+        chmod +x 'update.sh'
+        echo "alias aiw=/$HOME/AIW/aiwdebianubuntu.sh" >> .bashrc
+        echo "[Desktop Entry]
+        Name=All Is Well
+        GenericName=Update Helper and Fixer
+        Comment=ALL IS WELL is a bash script that allows users to easily update and upgrade their repositories and packages on linux, BSD, And More. It will also fix any broken packages and dependencies, if possible. Forget about typing all update/upgrade commands  manually, when ALL IS WELL!
+        Exec=$HOME/AIW/aiwdebianubuntu.sh
+        Icon=$HOME/AIW/icon-64.png
+        Terminal=true
+        StartupNotify=true
+        Type=Application
+        Categories=Utility;" > ~/.local/share/applications/aiw.desktop || error "Failed to create menu entry!"
+        clear
+        echo "Update Complete :)"
+        break
+        ;;
+    2)
+        echo "Updating All Is Well Script For Arch Linux"
+        mkdir ~/AIW
+        cd ~/AIW || error "Failed to enter directory 'AIW'!"
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/ALL%20IS%20WELL%20(V2.0)/icon-64.png'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/ALL%20IS%20WELL%20(V2.0)/aiwarchlinux.sh'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/uninstall.sh'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/LICENSE'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/update.sh'
+        chmod +x 'aiwarchlinux.sh'
+        chmod +x 'uninstall.sh'
+        chmod +x 'update.sh'
+        echo "alias aiw=/$HOME/AIW/aiwarchlinux.sh" >> .bashrc
+        echo "[Desktop Entry]
+        Name=All Is Well
+        GenericName=Update Helper and Fixer
+        Comment=ALL IS WELL is a bash script that allows users to easily update and upgrade their repositories and packages on linux, BSD, And More. It will also fix any broken packages and dependencies, if possible. Forget about typing all update/upgrade commands  manually, when ALL IS WELL!
+        Exec=$HOME/AIW/aiwarchlinux.sh
+        Icon=$HOME/AIW/icon-64.png
+        Terminal=true
+        StartupNotify=true
+        Type=Application
+        Categories=Utility;" > ~/.local/share/applications/aiw.desktop || error "Failed to create menu entry!"
+        clear
+        echo "Update Complete :)"
+        break
+        ;;
+    3)
+        echo "Updating All Is Well Script For Fedora"
+        mkdir ~/AIW
+        cd ~/AIW || error "Failed to enter directory 'AIW'!"
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/ALL%20IS%20WELL%20(V2.0)/icon-64.png'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/ALL%20IS%20WELL%20(V2.0)/aiwfedora.sh'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/uninstall.sh'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/LICENSE'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/update.sh'
+        chmod +x 'aiwfedora.sh'
+        chmod +x 'uninstall.sh'
+        chmod +x 'update.sh'
+        echo "alias aiw=/$HOME/AIW/aiwfedora.sh" >> .bashrc
+        echo "[Desktop Entry]
+        Name=All Is Well
+        GenericName=Update Helper and Fixer
+        Comment=ALL IS WELL is a bash script that allows users to easily update and upgrade their repositories and packages on linux, BSD, And More. It will also fix any broken packages and dependencies, if possible. Forget about typing all update/upgrade commands  manually, when ALL IS WELL!
+        Exec=$HOME/AIW/aiwfedora.sh
+        Icon=$HOME/AIW/icon-64.png
+        Terminal=true
+        StartupNotify=true
+        Type=Application
+        Categories=Utility;" > ~/.local/share/applications/aiw.desktop || error "Failed to create menu entry!"
+        clear
+        echo "Update Complete :)"
+        break
+        ;;
+    4)
+        echo "Updating All Is Well Script For Cent OS"
+        mkdir ~/AIW
+        cd ~/AIW || error "Failed to enter directory 'AIW'!"
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/ALL%20IS%20WELL%20(V2.0)/icon-64.png'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/ALL%20IS%20WELL%20(V2.0)/aiwcentos.sh'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/uninstall.sh'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/LICENSE'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/update.sh'
+        chmod +x 'aiwcentos.sh'
+        chmod +x 'uninstall.sh'
+        chmod +x 'update.sh'
+        echo "alias aiw=/$HOME/AIW/aiwcentos.sh" >> .bashrc
+        echo "[Desktop Entry]
+        Name=All Is Well
+        GenericName=Update Helper and Fixer
+        Comment=ALL IS WELL is a bash script that allows users to easily update and upgrade their repositories and packages on linux, BSD, And More. It will also fix any broken packages and dependencies, if possible. Forget about typing all update/upgrade commands  manually, when ALL IS WELL!
+        Exec=$HOME/AIW/aiwcentos.sh
+        Icon=$HOME/AIW/icon-64.png
+        Terminal=true
+        StartupNotify=true
+        Type=Application
+        Categories=Utility;" > ~/.local/share/applications/aiw.desktop || error "Failed to create menu entry!"
+        clear
+        echo "Update Complete :)"
+        ;;
+    5)
+        echo "Updating All Is Well Script For Solus OS"
+        mkdir ~/AIW
+        cd ~/AIW || error "Failed to enter directory 'AIW'!"
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/ALL%20IS%20WELL%20(V2.0)/icon-64.png'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/ALL%20IS%20WELL%20(V2.0)/aiwsolus.sh'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/uninstall.sh'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/LICENSE'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/update.sh'
+        chmod +x 'aiwsolus.sh'
+        chmod +x 'uninstall.sh'
+        chmod +x 'update.sh'
+        echo "alias aiw=/$HOME/AIW/aiwsolus.sh" >> .bashrc
+        echo "[Desktop Entry]
+        Name=All Is Well
+        GenericName=Update Helper and Fixer
+        Comment=ALL IS WELL is a bash script that allows users to easily update and upgrade their repositories and packages on linux, BSD, And More. It will also fix any broken packages and dependencies, if possible. Forget about typing all update/upgrade commands  manually, when ALL IS WELL!
+        Exec=$HOME/AIW/aiwsolus.sh
+        Icon=$HOME/AIW/icon-64.png
+        Terminal=true
+        StartupNotify=true
+        Type=Application
+        Categories=Utility;" > ~/.local/share/applications/aiw.desktop || error "Failed to create menu entry!"
+        clear
+        echo "Update Complete :)"
+        break
+        ;;
+    6)
+        echo "Updating All Is Well Script For BSD"
+        mkdir ~/AIW
+        cd ~/AIW || error "Failed to enter directory 'AIW'!"
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/ALL%20IS%20WELL%20(V2.0)/icon-64.png'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/ALL%20IS%20WELL%20(V2.0)/aiwbsd.sh'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/uninstall.sh'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/LICENSE'
+        wget 'https://raw.githubusercontent.com/spectrumgamer75/All-is-well/master/update.sh'
+        chmod +x 'aiwbsd.sh'
+        chmod +x 'uninstall.sh'
+        chmod +x 'update.sh'
+        echo "alias aiw=/$HOME/AIW/aiwbsd.sh" >> .bashrc
+        echo "[Desktop Entry]
+        Name=All Is Well
+        GenericName=Update Helper and Fixer
+        Comment=ALL IS WELL is a bash script that allows users to easily update and upgrade their repositories and packages on linux, BSD, And More. It will also fix any broken packages and dependencies, if possible. Forget about typing all update/upgrade commands  manually, when ALL IS WELL!
+        Exec=$HOME/AIW/aiwbsd.sh
+        Icon=$HOME/AIW/icon-64.png
+        Terminal=true
+        StartupNotify=true
+        Type=Application
+        Categories=Utility;" > ~/.local/share/applications/aiw.desktop || error "Failed to create menu entry!"
+        clear
+        echo "Update Complete :)"
+        break
+        ;;
+    7)
+        echo "Update Cancelled"
+        exit
+        ;;
 
-
-
-
-	    break
-            ;;
-	"Quit")
-	    echo "User requested exit"
-	    exit
-	    ;;
-        *) echo "invalid option $REPLY";;
-    esac
-done
-
+    *) echo "invalid option '$number'";;
+esac
 ######################################################
 # SCRIPT AUTHOR: SPECTRUMGAMER75 (^̮^)               #
 # www.github.com/spectrumgamer75                     #
