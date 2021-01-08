b74599b9b6679e69c74d7216c08e1faeb1ea68e6
Spectrumgamer75 <72115696+spectrumgamer75@users.noreply.github.com>
2021-01-08 09:03:41 +0530

    Integrated update feature into the main script
    The update feature has been integrated into the main script itself, no more special update scripts will be needed because of this!

--- ALL IS WELL (V2.0)/aiwfedora.sh
+++ ALL IS WELL (V2.0)/aiwfedora.sh
@@ -11,17 +11,30 @@ echo -en '\n'
 echo '#JUST A SMALL SCRIPT FOR EASY UPDATE/UPGRADES ON LINUX (Fedora)'
 echo 'VERSION 2.0'                                               
                                                                        
-                                                                                                                                  
-
-                                                                             
 #SPACE
 echo -en '\n'
-#SPACE                                                                                                                                                                                                                                                                                                                   
+#SPACE
 echo -en '\n'
-sleep 5
-echo "Please enter your password, if any to start ッ" 
+
+function error {
+  echo -e "\\e[91m$1\\e[39m"
+  exit 1
+}
+
+echo "Checking for updates..."
+cd $HOME/All-is-well
+localhash="$(git rev-parse HEAD)"
+latesthash="$(git ls-remote https://github.com/spectrumgamer75/All-is-well HEAD | awk '{print $1}')"
+if [ "$localhash" != "$latesthash" ] && [ ! -z "$latesthash" ] && [ ! -z "$localhash" ];then
+echo "Out of date, updating now..."
+git pull https://github.com/spectrumgamer75/All-is-well HEAD || error 'Unable to update, please check your internet connect'
+else
+  echo "Up to date."
+fi
+
+echo "Please enter your password, if any to start ッ"
 echo -en '\n'
-#SPACE                                                       
+#SPACE                                  
 sudo dnf update --fix-missing && sudo dnf upgrade -y && sudo dnf dist-upgrade -y && sudo dnf autoremove -y && sudo dnf install -f && sudo rpm -Va
 echo -en '\n'
 #SPACE 
