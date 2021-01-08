d39286e86af7bc28ea6372dbd124f85616354ab7
Spectrumgamer75 <72115696+spectrumgamer75@users.noreply.github.com>
2021-01-08 08:51:15 +0530

    Integrated update feature into the main script
    The update feature has been integrated into the main script itself, no more special update scripts will be needed because of this!

--- ALL IS WELL (V2.0)/aiwdebianubuntu.sh
+++ ALL IS WELL (V2.0)/aiwdebianubuntu.sh
@@ -1,31 +1,44 @@
-#!/bin/bash      
+#!/bin/bash
 echo '
-   _____  .__  .__     .___           __      __       .__  .__   
+   _____  .__  .__     .___           __      __       .__  .__
   /  _  \ |  | |  |    |   | ______  /  \    /  \ ____ |  | |  |
  /  /_\  \|  | |  |    |   |/  ___/  \   \/\/   // __ \|  | |  |
 /    |    \  |_|  |__  |   |\___ \    \        /\  ___/|  |_|  |__
 \____|__  /____/____/  |___/____ >     \__/\  /  \___  >____/____/
-        \/                      \/          \/       \/            '                           
-                                            
-echo -en '\n'                                           
-echo '#JUST A SMALL SCRIPT FOR EASY UPDATE/UPGRADES ON LINUX (Debian and Ubuntu)'
-echo 'VERSION 2.0'      
-                                                                       
-                                                                                                                                  
+        \/                      \/          \/       \/            '
 
-                                                                             
+echo -en '\n'
+echo '#JUST A SMALL SCRIPT FOR EASY UPDATE/UPGRADES ON LINUX (Debian and Ubuntu)'
+echo 'VERSION 2.0'
+
 #SPACE
 echo -en '\n'
-#SPACE                                                                                                                                                                                                                                                                                                                   
+#SPACE
 echo -en '\n'
-sleep 5
-echo "Please enter your password, if any to start ッ" 
+
+function error {
+  echo -e "\\e[91m$1\\e[39m"
+  exit 1
+}
+
+echo "Checking for updates..."
+cd $HOME/All-is-well
+localhash="$(git rev-parse HEAD)"
+latesthash="$(git ls-remote https://github.com/spectrumgamer75/All-is-well HEAD | awk '{print $1}')"
+if [ "$localhash" != "$latesthash" ] && [ ! -z "$latesthash" ] && [ ! -z "$localhash" ];then
+echo "Out of date, updating now..."
+git pull https://github.com/spectrumgamer75/All-is-well HEAD || error 'Unable to update, please check your internet connect'
+else
+  echo "Up to date."
+fi
+
+echo "Please enter your password, if any to start ッ"
 echo -en '\n'
-#SPACE                                                       
+#SPACE
 sudo apt update --fix-missing && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y
 echo -en && sudo apt install -f && sudo dpkg --configure -a
-#SPACE 
-#Put a hash in front of "clear" to stop clearing the output 
+#SPACE
+#Put a hash in front of "clear" to stop clearing the output
 clear
 echo "All is well now (if everything went correctly) press x to exit 〷◠‿◠〷"
 count=0
