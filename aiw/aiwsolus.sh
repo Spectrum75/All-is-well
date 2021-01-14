fedcac2e0c3215fb680581656a022b20341686c9
Spectrumgamer75 <72115696+spectrumgamer75@users.noreply.github.com>
2021-01-14 11:43:45 +0530

    Added Error Message And Increased Spacing

--- aiw/aiwsolus.sh
+++ aiw/aiwsolus.sh
@@ -1,46 +1,44 @@
-#!/bin/bash      
+#!/bin/bash
 echo '
-    _____  .__  .__     .___           __      __       .__  .__   
+    _____  .__  .__     .___           __      __       .__  .__
    /  _  \ |  | |  |    |   | ______  /  \    /  \ ____ |  | |  |
   /  /_\  \|  | |  |    |   |/  ___/  \   \/\/   // __ \|  | |  |
  /    |    \  |_|  |__  |   |\___ \    \        /\  ___/|  |_|  |__
  \____|__  /____/____/  |___/____ >     \__/\  /  \___  >____/____/
          \/                      \/          \/       \/
 '
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
-echo -en '\n'                                           
-echo 'JUST A SMALL SCRIPT FOR EASY UPDATE/UPGRADES ON LINUX (Solus OS)
-VERSION 2.0'                                           
 
-#SPACE
 echo -en '\n'
-#SPACE
+echo -e "Just A Small Script For Updating/Upgrading And Fixing Packages On Linux \e[7m(Solus OS)\e[27m"
+echo 'Version 2.0'
+echo -en '\n'
 echo -en '\n'
 
 function error {
   echo -e "\\e[91m$1\\e[39m"
   exit 1
 }
-
-echo "Checking for updates..."
+#Update Check.
+echo -e "\e[31mChecking for updates...\e[0m"
 cd $HOME/All-is-well
 localhash="$(git rev-parse HEAD)"
 latesthash="$(git ls-remote https://github.com/spectrumgamer75/All-is-well HEAD | awk '{print $1}')"
 if [ "$localhash" != "$latesthash" ] && [ ! -z "$latesthash" ] && [ ! -z "$localhash" ];then
 echo "Out of date, updating now..."
-git pull https://github.com/spectrumgamer75/All-is-well HEAD || error 'Unable to update, please check your internet connect'
+git pull https://github.com/spectrumgamer75/All-is-well HEAD || error '\e[31mUnable to update, please check your internet connection...\e[0m'
 else
-  echo "Up to date."
+  echo -e "\e[32mUp to date! \e[0m"
 fi
-
 echo "Please enter your password, if any to start ッ"
 echo -en '\n'
-#SPACE                                             
-sudo eopkg up && sudo eopkg check | grep Broken | awk '{print $4}' | xargs sudo eopkg it --reinstall && sudo eopkg upgrade && sudo eopkg ur && eopkg check
-#SPACE 
-#Put a hash in front of "clear" to stop clearing the output 
-clear
-echo "All is well now (if everything went correctly) press x to exit ◠‿◠"
+# Main:
+sudo eopkg up || error '\e[31mUnable to update, please check your internet connection...\e[0m' && sudo eopkg check | grep Broken | awk '{print $4}' | xargs sudo eopkg it --reinstall && sudo eopkg upgrade && sudo eopkg ur && eopkg check
+#Comment 'clear' below to stop clearing the output.
+#clear
+echo -en '\n'
+echo -en '\n'
+echo -en '\n'
+echo "All is well now (if everything went correctly) press x to exit 〷◠‿◠〷"
 count=0
 while : ; do
 read -n 1 k <&1
@@ -50,15 +48,15 @@ break
 else
 ((count=$count+1))
 printf "\nWrong key pressed for $count times\n"
-echo "Press 'x' to exit (ʘ‿ʘ)"
+echo "All is well now press x to exit ◠‿◠"
 fi
 done
 
 # About this Script:
 # ALL IS WELL is a bash script that allows users to easily update and upgrade their repositories and packages on linux, BSD, And More. It will also fix any broken packages and dependencies, if possible. Forget about typing all update/upgrade commands  manually, when ALL IS WELL!
 
-#####################################################
-# SCRIPT AUTHOR: SPECTRUMGAMER75 (^̮^)               #
-# www.github.com/spectrumgamer75                    #
-# Incase of any errors kindly let me know on GitHub.#
-#####################################################
+######################################################
+# SCRIPT AUTHOR: SPECTRUMGAMER75 (^̮^)                #
+# www.github.com/spectrumgamer75                     #
+# Incase of any errors kindly let me know on GitHub. #
+######################################################
