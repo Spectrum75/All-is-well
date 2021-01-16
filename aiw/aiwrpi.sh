e52b9968b54d7c742c9f5573172d514098a9d0f6
Spectrumgamer75 <72115696+spectrumgamer75@users.noreply.github.com>
2021-01-16 15:19:45 +0530

    Changed Emoji For Raspberry Pi OS
    Emoji for raspberry pi os has been changed for better display campatibility.

--- aiw/aiwrpi.sh
+++ aiw/aiwrpi.sh
@@ -50,7 +50,7 @@ break
 else
 ((count=$count+1))
 printf "\nWrong key pressed for $count times\n"
-echo "All is well now press x to exit ◠‿◠"
+echo "All is well now press x to exit (•‿•)"
 fi
 done
 
